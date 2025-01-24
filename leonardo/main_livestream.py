7ca26b1b3c72aea3b9ebe368838260908e4a0bfd
danielSbastos <daniel.bastos1710@gmail.com>
2025-01-24 16:16:27 -0300

    TRigger recognition after 4 seconds

--- leonardo/main_livestream.py
+++ leonardo/main_livestream.py
@@ -3,41 +3,25 @@
 # media de classificacao de X frames 
 
 import mediapipe as mp
-
 import time #import time, sleep, time_ns
-import random
-
 import cv2
-import xml.etree.ElementTree as ET
 from pyo import *
 import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
 from playsound import playsound
-from collections import Counter
-
+import datetime
 import os
 import absl.logging
 import sys
 import tensorflow as tf
 
-from pyo import *
+import random
 
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Create a red screen
 absl.logging.set_verbosity(absl.logging.ERROR)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 0 = all logs, 1 = INFO, 2 = WARNING, 3 = ERROR
 sys.stderr = open(os.devnull, 'w')
 os.environ["MESA_DEBUG"] = "0"
 os.environ["MESA_LOG_LEVEL"] = "fatal"
 tf.get_logger().setLevel('INFO')
-
-s = Server()
-s.setOutputDevice(0)
-s.boot()
-s.start()
 
 def play_soco():
     playsound("soco.wav", block=False)
@@ -57,6 +41,26 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 global inside_min_distance
 inside_min_distance = False
 
+global begun_playing
+begun_playing = None
+
+def play(gesture):
+    global begun_playing
+
+    if begun_playing is None:
+        begun_playing = time.time()
+    
+    delta = time.time() - begun_playing
+    ran = random.uniform(0, 1)
+
+    if ran < 0.3 and delta > 4:
+        begun_playing = None
+        print(f"PLAY: {ran} - GESTURE: {gesture}")
+        return True
+
+    print(f"NOT PLAY: {ran} - GESTURE: {gesture}")
+    return False
+
 def print_result(result, output_image: mp.Image, timestamp_ms: int):
     hand_landmarks = result.hand_landmarks
 
@@ -71,25 +75,25 @@ def print_result(result, output_image: mp.Image, timestamp_ms: int):
         best_gesture = result.gestures
         best_gesture.sort(key=lambda x: x[0].score, reverse=True)
 
-        current_inside_min_distance = best_gesture[0][0].score > 0.66 and distance < 0.14 and left_and_right == ['Left', 'Right'] 
-
+        current_inside_min_distance = best_gesture[0][0].score > 0.60 and distance < 0.4 and left_and_right == ['Left', 'Right'] 
         global inside_min_distance
 
         if current_inside_min_distance and inside_min_distance:
-            print("INSIDE")
             pass # do nothing
-        
+
         if (not inside_min_distance) and current_inside_min_distance:
-            print("ENTERED")
             gesture = best_gesture[0][0].category_name
-            if gesture == 'soco':
-                play_soco()
-            elif gesture == 'vira':
-                play_vira()
-            elif gesture == 'bate':
-                play_bate()
+            if gesture in ['soco', 'bate']:
+                play(gesture)
+            #if gesture == 'soco':
+            #    if play():
+            #        play_soco()
+            #elif gesture == 'vira':
+            #    play_vira()
+            #elif gesture == 'bate':
+            #    play_bate()
             inside_min_distance = True
-        
+            
         if not current_inside_min_distance:
             inside_min_distance = False
 
@@ -107,10 +111,19 @@ def transform_coordinates(set_gestures_coordinates, i):
         _x, _y, _z = thumb_mark.x, thumb_mark.y, thumb_mark.z
         coords.append([_x, _y, _z])
 
-    dist0_1 = distance(coords, 0, 1)
-    return dist0_1
+    dists = []
+    if len(coords) == 2:
+        dists.append(distance(coords, 0, 1))
+    elif len(coords) == 3:
+        dists.append(distance(coords, 0, 1))
+        dists.append(distance(coords, 0, 2))
+        dists.append(distance(coords, 2, 1))
 
-cap = cv2.VideoCapture(2)
+    if len(dists) == 0:
+        return 1
+    return min(dists) 
+
+cap = cv2.VideoCapture(0)
 
 global mirrored_frame
 last_gesture = ''
@@ -119,8 +132,8 @@ ct_results = []
 while cap.isOpened():
     options = GestureRecognizerOptions(
         base_options=BaseOptions(model_asset_path='./exported_model/gesture_recognizer.task'),
-        num_hands=4,
-        min_hand_detection_confidence=0.7,
+        num_hands=2,
+        min_hand_detection_confidence=0.6,
         running_mode=VisionRunningMode.LIVE_STREAM,
         result_callback=print_result)
     with GestureRecognizer.create_from_options(options) as recognizer:
@@ -129,13 +142,14 @@ while cap.isOpened():
             continue
         
         mirrored_frame = cv2.flip(frame, 1)
-        combined_frame = cv2.hconcat([frame, mirrored_frame])
-        image_rgb = cv2.cvtColor(combined_frame, cv2.COLOR_BGR2RGB)
+        #combined_frame = cv2.hconcat([mirrored_frame, frame])
+        #image_rgb = cv2.cvtColor(combined_frame, cv2.COLOR_BGR2RGB)
+        image_rgb = cv2.cvtColor(mirrored_frame, cv2.COLOR_BGR2RGB)
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image_rgb)
 
         recognizer.recognize_async(mp_image, mp.Timestamp.from_seconds(time.time()).value)
 
-        cv2.imshow('Hand Gesture Recognition', combined_frame)
+        cv2.imshow('Hand Gesture Recognition',mirrored_frame)
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
